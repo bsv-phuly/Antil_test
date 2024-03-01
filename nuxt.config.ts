6b0cba1998889e681f50e831174d9717f22ee6d1
bsv Phu Ly <phuly@bravesoft.com.vn>
2024-03-01 16:05:54 +0700

    init package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    // baseURL: '/Antil_test/', // baseURL: '/<repository>/'
-    // buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+    baseURL: '/Antil_test/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
   modules: [
     'nuxt-typed-router',
