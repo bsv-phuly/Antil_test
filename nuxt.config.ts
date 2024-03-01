72d005b95a07435ee5aff64798e3379fcde5aedb
bsv Phu Ly <phuly@bravesoft.com.vn>
2024-03-01 16:19:04 +0700

    fix _payload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    payloadExtraction: true
+  },
   app: {
     baseURL: '/Antil_test/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
